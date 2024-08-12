188e2732ca6e18b216a4122857402c9614ea13be
Talha-git2450 <313talhahunter@gmail.com>
2024-08-12 12:47:40 +0000

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3)// we are using node jd not browser
+
+console.log(3+
+    3)//code must be in readable form
+
+    console.log("Talha")
