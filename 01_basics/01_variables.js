7073b73b6e6b5f8a39acd6fd4aca7de72ec83ec2
Talha-git2450 <313talhahunter@gmail.com>
2024-08-12 07:21:23 +0000

    changes is done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const person_Id=782124
+let  person_Email="Talha@gmail.com"
+var person_Password="2450"
+person_city= "Mumbai"
+let person_State="MAHARASHTRA";
+
+//person_Id=421287
+person_Email="Talha@hotmail.com"
+person_Password="7821"
+
+/*console.log(person_Id);
+
+console.log(person_Email);
+
+console.log(person_Password);
+
+console.log(person_city);*/
+
+// We didn't use var keyword in JS
+
+console.table([person_Id, person_Email, person_Password, person_city, person_State])
+
